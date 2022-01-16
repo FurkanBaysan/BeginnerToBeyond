51e37d5740064081c943acb38b556df6c5f2d17a
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-01-17 01:37:26 +0300

    Add project files.

---
+++ BeginnerToBeyond/BeginnerToBeyond.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int favouriteNumber;
+	cout << "Enter Your Favourite Number between 1-100:";
+	cin >> favouriteNumber;
+	cout << "Amazing! That's my favourite number too!!" << endl;
+	return 0;
+}
---
+++ GettingStartedChallengeSolution/ChallengeSolution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\t" << "--Challenge Solution--" << endl;
+	int favouriteNumber;
+	cout << "Enter your Favourite Number:";
+	cin >> favouriteNumber;
+	cout << "Amazing!! That's my favourite number too!" << endl;
+	cout << "No Really!!" << " " << favouriteNumber << " " << "is my favourite number";
+	return 0;
+}

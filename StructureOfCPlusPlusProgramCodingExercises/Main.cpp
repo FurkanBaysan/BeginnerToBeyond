e79f79779c6335b930f1dd4d218657e38bc3ece8
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-01-19 08:50:29 +0300

    Structure of C++ Program Exercise Solutions

---
+++ StructureOfCPlusPlusProgramCodingExercises/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void printName();
+void printTheNumberOfDogs();
+void printDateOfBirth();
+int main() {
+	cout << "\t" << "--Solution:Coding Exercise 1--" << endl;
+	printName();
+	cout << "\t" << "--Solution:Coding Exercise 2--" << endl;
+	printTheNumberOfDogs();
+	cout << "\t" << "--Solution:Coding Exercise 3--" << endl;
+	printDateOfBirth();
+	return 0;
+}
+void printName() {
+	cout << "Hi Furkan!" << endl;
+}
+void printTheNumberOfDogs() {
+	int numberOfDogs = 3;
+	cout << "Sally has " << numberOfDogs << " dogs." << endl;
+}
+void printDateOfBirth() {
+	int month;
+	int day;
+	int year;
+	cout << "Enter your Date of Birth of type day/month/year:";
+	cin >> day >> month >> year;
+	cout << day << " " << month << " " << year << endl;
+}
